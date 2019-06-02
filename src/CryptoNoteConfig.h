7b70277bb821f6c1173b4b35f1b973e7a803d784
Dabnee <48941487+Dabnee@users.noreply.github.com>
2019-06-01 19:10:32 -0700

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -85,9 +85,9 @@ const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  200;    //by def
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
 //TODO This port will be used by the daemon to establish connections with p2p network
-const int      P2P_DEFAULT_PORT                              = 17454;
+const int      P2P_DEFAULT_PORT                              = 18454;
 //TODO This port will be used by the daemon to interact with simlewallet
-const int      RPC_DEFAULT_PORT                              = 18454;
+const int      RPC_DEFAULT_PORT                              = 19454;
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"165.227.58.71:17454",
-  //"159.65.110.147:17454",
+  //"165.227.58.71:18454",
+  //"159.65.110.147:18454",
 };
 
 struct CheckpointData {
